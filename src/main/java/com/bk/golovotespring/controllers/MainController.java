c88e3bdf061d4010274e1c2f6b65af4917bda3b0
BaoKhanh99 <tlbk1999@gmail.com>
2022-01-18 18:14:37 +0700

    add html

--- src/main/java/com/bk/golovotespring/controllers/MainController.java
+++ src/main/java/com/bk/golovotespring/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.bk.golovotespring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,5 +24,28 @@ public class MainController {
         return "fuck";
     }
 
+    @GetMapping("/position-list")
+    public String positionListPage(){
+        return "views/positions-list";
+    }
 
+    @GetMapping("/candidate-list/{position}")
+    public String CandidateListPage(@PathVariable("position")String position){
+        return "views/candidate-list";
+    }
+
+    @GetMapping("/successful-vote-page")
+    public String SuccessfulVotePage(){
+        return "views/successful-vote-page";
+    }
+
+    @GetMapping("/voted-list")
+    public String votedList(){
+        return "views/voted-list";
+    }
+
+    @GetMapping("/ranking-page")
+    public String rankingPage(){
+        return "views/ranking-page";
+    }
 }
